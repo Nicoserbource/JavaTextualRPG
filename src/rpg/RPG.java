047aed373bd49a009269f860f96ff8a5e4686d4e
Sicolan <nicolas.serbource@gmail.com>
2020-09-17 15:35:01 +0200

    Refactor main class

--- src/rpg/RPG.java
+++ src/rpg/RPG.java
@@ -14,24 +14,18 @@ public class RPG {
 		joueur.afficherStats();
 		System.out.println("Bienvenue, "+nomJoueur+" ! Vous pouvez commencer votre aventure.");
 		input.nextLine();
-		Monstre loupFaible = new Monstre("loup", 4, 4, 1, 0, 2, 1);
-		Combat.combattreAuto(joueur, loupFaible);
-		input.nextLine();
-		Monstre banditFaible = new Monstre("bandit", 5, 5, 1, 1, 1, 1);
-		Combat.combattreAuto(joueur, banditFaible);
-		input.nextLine();
-		Monstre loupMoyen = new Monstre("loup", 6, 6, 2, 1, 4, 2);
-		Combat.combattreAuto(joueur, loupMoyen);
-		input.nextLine();
-		Monstre banditMoyen = new Monstre("bandit", 10, 10, 3, 2, 2, 2);
-		Combat.combattreAuto(joueur, banditMoyen);
-		input.nextLine();
-		Monstre loupFort = new Monstre("loup", 8, 8, 3, 2, 6, 3);
-		Combat.combattreAuto(joueur, loupFort);
-		input.nextLine();
-		Monstre banditFort = new Monstre("bandit", 15, 15, 5, 3, 3, 3);
-		Combat.combattreAuto(joueur, banditFort);
-		input.nextLine();
+		Monstre[] tableauMonstres = {
+	   		new Monstre("loup", 5, 5, 1, 0, 2, 1),
+	   		new Monstre("bandit", 5, 5, 1, 1, 1, 1),
+	   		new Monstre("loup", 6, 6, 2, 0, 3, 1),
+	   		new Monstre("bandit", 10, 10, 2, 2, 2, 1),
+	   		new Monstre("loup", 7, 7, 3, 0, 4, 1),
+	   		new Monstre("bandit", 15, 15, 3, 3, 3, 1)
+		};
+		for(Monstre monstre : tableauMonstres) {
+			Combat.combattreAuto(joueur, monstre);
+			input.nextLine();
+		}
 		System.out.println("Vous avez terminé le premier niveau.");
 		input.close();
 	}
